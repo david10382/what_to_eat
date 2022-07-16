d5fd23edc4000b26c540a124aac091cad5d029d2
david10382 <105527202+david10382@users.noreply.github.com>
2022-07-16 22:16:43 +1000

    adding main_app.py

---
+++ main_app.py
@@ -0,0 +1,14 @@
+import inquirer
+
+home_questions = [
+  inquirer.List('home_page',
+                message="Welcome! Let's find you something to eat",
+                choices=['Selections', 'Quick Search', 'Exit'],
+            ),
+]
+answers_home = inquirer.prompt(home_questions)
+
+def search_user_answer(user_ans):
+  user_ans = answers_home['home_page'].lower()
+
+search_user_answer(answers_home)
