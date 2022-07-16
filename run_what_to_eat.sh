ac028a8a0e3b185b28cea4d7a84e938b5c5c62ab
david10382 <105527202+david10382@users.noreply.github.com>
2022-07-17 00:40:12 +1000

    adding bash logic to run python

--- run_what_to_eat.sh
+++ run_what_to_eat.sh
@@ -1,1 +1,10 @@
-python3 main_app.py
+if [[ $# -eq 1 ]]; then
+    echo "There are parameters"
+    if [[ "$1" == "run" ]]; then
+        echo "Start main_app.py"
+        python3 main_app.py
+    fi
+    
+else 
+    echo "No parameter"
+fi
