8a159d086997d5e635dd1f14ce95e412fab0ed42
david10382 <105527202+david10382@users.noreply.github.com>
2022-07-16 22:26:15 +1000

    adding data file

---
+++ data.py
@@ -0,0 +1,122 @@
+menu = {
+        'Thai Grilled Chicken' : {
+            'main_category': 'south asian',
+            'sub_category': 'rice',
+            'ingredient': 'chicken, green onion, oyster sauce, soy sauce, vegetable oil, sweet soy sauce, salt, pepper, sugar, cucumber',
+            'allergies' : 'meat'
+            }, 
+       'Thai Grilled Fish' : {
+            'main_category': 'south asian',
+            'sub_category': 'rice',
+            'ingredient': 'fish, green onion, oyster sauce, soy sauce, vegetable oil, sweet soy sauce, salt, pepper, sugar, cucumber',
+            'allergies' : 'seafood'
+            }, 
+      'Hamburger' : {
+            'main_category': 'north american',
+            'sub_category': 'bread',
+            'ingredient': 'beef mince, onion, egg, garlic, olive oil, cheese,lettuce, tomato',
+            'allergies' : 'gluten, meat'
+            },
+       'Udon': {
+            'main_category': 'east asian',
+            'sub_category': 'noodle, soup',
+            'ingredient': 'noodles, dashi soup, salt, sugar, light soy sauce, spring onion, mirin, bonito flakes',
+            'allergies' : 'gluten, seafood'
+            },
+        'Yangzhou Fried Rice' : {
+            'main_category': 'east asian',
+            'sub_category': 'rice',
+            'ingredient': 'carrot, peas, pork, shrimp, egg, scallion',
+            'allergies' : 'seafood, meat'
+            },
+        'Vegetarian Bibimbap' : {
+            'main_category': 'east asian',
+            'sub_category': 'rice',
+            'ingredient': 'vegetarian, korean sweet chilli paste, bean sprouts, carrots, mushrooms, spinach, sesame oil',
+            'allergies' : 'nuts'  
+            },
+        'Pho' : {
+            'main_category': 'south asian',
+            'sub_category': 'noodle, soup',
+            'ingredient': 'bone broth soup, onion, garlic, ginger, cloves, cinnamon, caradamom, fennel seeds, coriander seeds, salt, fish sauce, sugar, staranise, beef, noodle',
+            'allergies' : 'gluten, meat'
+            },
+        'Vongole Pasta' : {
+            'main_category': 'europe',
+            'sub_category': 'noodle',
+            'ingredient': 'spaghetti, olive oil, garlic, clams(seafood), white wine, parsley',
+            'allergies' : 'seafood, gluten'
+            },
+        'Margherita Pizza' : {
+            'main_category': 'europe',
+            'sub_category': 'bread',
+            'ingredient': 'vegetarian, extra virgin olive oil, mozzarella, basil, tomato sauce, pizza dough',
+            'allergies' : 'gluten, dairy, meat'
+            },
+        'Ratatouille' : {
+            'main_category': 'europe',
+            'sub_category': 'none',
+            'ingredient': 'vegetarian, eggplant, tomato, yellow squash, zucchini, olive oil, garlic, red bell pepper, yellow bell pepper, basil',
+            'allergies' : ''
+            },
+        'Hungarian Ghoulash' : {
+            'main_category': 'eastern europe',
+            'sub_category': 'soup',
+            'ingredient': 'pork lard, onion, paprika, beef, garlic, red bell peppers, yellow bell peppers, tomatoes, carrots, potato, bay leaf, salt, black pepper, caraway seed',
+            'allergies' : 'gluten, meat'
+            },
+        'Chicken Kiev' :{
+            'main_category': 'eastern europe',
+            'sub_category': 'none',
+            'ingredient': 'butter, garlic, salt, parsley, chicken breast, breadcrumbs, flour, egg',
+            'allergies' : 'gluten, meat'
+            },
+        'Borscht Soup' : {
+            'main_category': 'eastern europe',
+            'sub_category': 'soup',
+            'ingredient': 'beets, chicken broth, onion, potato, bay leaves, white vinegar, garlic, dill, tomato, beans',
+            'allergies' : 'gluten, meat'
+            },
+        'Feijoada' : {
+            'main_category': 'south american',
+            'sub_category': 'rice',
+            'ingredient': 'black beans, olive oil, bacon, pork ribs, chorizo sausage, onoin, garlic, tomato, salt , black pepper, bay leaves',
+            'allergies' : 'gluten, meat'
+            },
+        'Vaca Atolada' : {
+            'main_category': 'south america',
+            'sub_category': 'soup',
+            'ingredient': 'beef ribs, garlic, pepper, onoin, tomato, bay leaves, parsley, cassava',
+            'allergies' : ' meat'
+            },
+        'Bauru': {
+            'main_category': 'south america',
+            'sub_category': 'bread',
+            'ingredient': 'french roll bread, melted ceese, pickles, tomatoes, beef',
+            'allergies' : 'meat'
+            },
+        'Poutine' : {
+            'main_category': 'north american',
+            'sub_category': 'none',
+            'ingredient': 'beef gravy, potatoes, cheese curds, vegetable oil',
+            'allergies' : 'meat'
+            },
+        'fish and chips': {
+            'main_category': 'european',
+            'sub_category': 'none',
+            'ingredient': 'flour, baking powder, salt, fish, potato',
+            'allergies' : 'gluten, fish'
+            },
+        'Fish Taco': {
+            'main_category': 'north american',
+            'sub_category': 'bread',
+            'ingredient': 'lime, olive oil, paprika, cumin, salt, avocado, cilantro, corn, cabbage, tomato, fish, tortilla',
+            'allergies' : 'gluten, meat'
+            },
+        'Chickpea Curry' : {
+            'main_category': 'south asian',
+            'sub_category': 'rice',
+            'ingredient': 'vegetarian, onion, tomato, garam masala, coriander, ginger, garlic, chickpea, coconut',
+            'allergies' : ''
+            }
+}
