17af655aa6d10f7d9d55b3bce8305117f1292fc3
david10382 <105527202+david10382@users.noreply.github.com>
2022-07-16 23:58:10 +1000

    adding return to main page function

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from data import menu
 import difflib
 from difflib import SequenceMatcher
 from difflib import get_close_matches
+import homepage 
 
 keys = menu.keys()
 
 
 def get_menu(word):
     word = word.title()
-    # print("get_close_matches(word, keys)",get_close_matches(word, keys))
     if word in menu or len(get_close_matches(word, keys)) > 0:
         if len(get_close_matches(word, keys)) > 0:
             user_input = input("Did you mean %s ? Enter Y for Yes, or N for No: " % get_close_matches(word, menu.keys())[0])
@@ -18,42 +18,20 @@ def get_menu(word):
                 print("Ingredient for ",word," are ...")
                 print (search_result['ingredient'])
                 print("Bon Appétit !!!")
+            elif user_input == "N":
+                print("let go back to main_app")
+                homepage.init_main_page()
+
     else:
         print('unknown menu item, please try again.')
         init_get_menu()
+    
 
 
 def init_get_menu():
     menu_item = input('what menu item do you want to check ingredients?:')
-    # print (get_menu(menu_item))
     get_menu (menu_item)
+    
 
-#user_main_category = input("input main_category : ")
-
-#for menu_type in menu:
-    #print("this is menu[menu_type]",menu[menu_type])
-    #for key in menu[menu_type]:
-       # print("THIS IS KEY ",key)
-        #if user_main_category.lower() == menu[menu_type][key].lower():
-            #print(menu[menu_type])
-            #print(menu[menu_type][key].lower())
-            #print("\nWould you like to have :", menu_type)
-#menu = recipes.load(open('data.py'))
-#sub_category_answer = input("what would you like to eat?: ")
-#print('ans is: ',sub_category_answer)
-
-#if sub_category_answer.lower() == "thai":
-    #print("It is thai")
-    #for index in recipes:
-     #   for nestedindex in recipes[index]: 
-      #      if nestedindex.lower() == "sub_category":
-       #         if recipes[index][nestedindex].lower() == "thai":
-        #            print(recipes[index]['name'])
-                    #print(nestedindex, ":",recipes[index][nestedindex])6
-#else:
- #   print(" not thai")
-
- #def get_meal(word):
-    #return menu[word]
 
 
--- main_app.py
+++ main_app.py
@@ -1,23 +1,4 @@
-import inquirer
-from welcome import init_welcome
-from app import init_get_menu
-from goodbye import goodbye
+from homepage import init_main_page
 
-home_questions = [
-  inquirer.List('home_page',
-                message="Welcome! Let's find you something to eat",
-                choices=['Selections', 'Menu Item Ingredients', 'Exit'],
-            ),
-]
-answers_home = inquirer.prompt(home_questions)
 
-def search_user_answer(user_ans):
-  user_ans = answers_home['home_page'].lower()
-  if user_ans == 'selections':
-    init_welcome()
-  elif user_ans == 'quick search':
-    init_get_menu()
-  elif user_ans == 'exit':
-    goodbye()
-
-search_user_answer(answers_home)
+init_main_page()
