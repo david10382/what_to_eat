e98adab9314b16e227d6a49f9c5006c4033bdc87
david10382 <105527202+david10382@users.noreply.github.com>
2022-07-16 23:13:25 +1000

    change name of quick search to menu item ingredients

--- main_app.py
+++ main_app.py
@@ -6,7 +6,7 @@ from goodbye import goodbye
 home_questions = [
   inquirer.List('home_page',
                 message="Welcome! Let's find you something to eat",
-                choices=['Selections', 'Quick Search', 'Exit'],
+                choices=['Selections', 'Menu Item Ingredients', 'Exit'],
             ),
 ]
 answers_home = inquirer.prompt(home_questions)
